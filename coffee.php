04c712078c208245cc29a94476999693612a0605
paulmlaviwa <mlaviwa.paul@gmail.com>
2023-11-23 15:10:58 -0500

    Dzukani, caffein wake ndiwamphaaaaaamvuuu

--- coffee.php
+++ coffee.php
@@ -1,12 +1,29 @@
 <?php
-$coffee = new coffee();
-if(empty($coffe))
-{
-	$coffe->refill();
+
+class Coffee {
+    private $isFull = false;
+
+    public function refill() {
+        $this->isFull = true;
+        echo "Coffee has been refilled.\n";
+
+    public function drink() {
+        if ($this->isFull) {
+            $this->isFull = false;
+            echo "TopDev gurus have drunk their coffee. Time to code!\n";
+        } else {
+            echo "The reservoir is empty. Time to refill!\n";
+        }
+    }
 }
-else
-{
-	$coffe->drink();
+}
+
+$coffee = new Coffee();
+
+if (empty($coffee)) {
+    $coffee->refill();
+} else {
+    $coffee->drink();
 }
 
 ?>
